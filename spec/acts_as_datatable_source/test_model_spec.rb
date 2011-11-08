7581e08df4638428e760f0f953478d72214a9f36
Meredith Lesly <bitcababy+github@gmail.com>
2011-11-08 16:03:15 -0500

    Added tests for #construct_for_json

--- spec/acts_as_datatable_source/test_model_spec.rb
+++ spec/acts_as_datatable_source/test_model_spec.rb
@@ -47,4 +47,57 @@ describe TestModel do
 		end
 	end
 
+	describe "#construct_for_json" do
+		before :each do
+			@models = []
+			@models << Factory(:test_model, :foo => 'b', :bar => 2)
+			@models << Factory(:test_model, :foo => 'a', :bar => 3)
+		end
+		
+		describe "no sorting" do
+			before :each do
+				@opts['iSortingCols'] = 0
+				@res = TestModel.construct_for_json(@opts)
+			end
+				
+			it "returns a hash" do
+				@res.should be_kind_of Hash
+			end
+			it "should contain 'sEcho', 'iTotalRecords', 'iTotalDisplayRecords', 'sColumns', and 'aaData' " do
+				%W(sEcho iTotalRecords iTotalDisplayRecords sColumns aaData).each do |field|
+					@res[field].should_not be_nil
+				end
+			end
+			it "contains 'sEcho', which should match the value from @opts" do
+				@res['sEcho'].should == @opts['sEcho']
+			end
+			it "contains 'iTotalRecords', which should match the total number of records" do
+				@res['iTotalRecords'].should == 2
+			end
+			it "contains 'iTotalDisplayRecords', which should match the number of records displayed" do
+				@res['iTotalDisplayRecords'].should == 2
+			end
+			it "contains 'sColumns', which should match sName" do
+				@res['sColumns'].should == @columns.join(",")
+			end
+			describe "aaData" do
+				before :each do
+					@aaData = @res['aaData']
+				end
+				it "should contain an array for each record" do
+					@aaData.should be_kind_of Array
+					@aaData.count.should == @res['iTotalDisplayRecords']
+					cols = @res['sColumns']
+					# We can test this way because of no sorting
+					@res['iTotalDisplayRecords'].times do |i|
+						model = @models[i]
+						@aaData[i].should == cols.split(",").collect {|col| model[col]}
+					end
+				end
+			end
+		end # no sorting
+			
+	end
+	
+
 end

251b07c4f79897f148becb1d85834f2f8506080a
Meredith Lesly <bitcababy+github@gmail.com>
2011-11-19 14:24:45 -0500

    Don't need these.

--- spec/dummy/app/models/test_model.rb
+++
@@ -1,9 +0,0 @@
-class TestModel < ActiveRecord::Base
-	acts_as_datatable_source
-	
-	def bar_quux
-		"#{self.read_attribute('bar').to_s}/#{self.read_attribute('quux').to_s}"
-	end
-	
-end
-
--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '5e894084b7e020d9c9a6fed131afd28dc6e94551f6ce637efd221f3fea342a8d6d97df816e67ce9b2c3e1f5de9a24859af5b0c5dabf19308398881aeeb784f61'

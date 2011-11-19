a34d756d95e8f888473868722f6396533cda568f
Meredith Lesly <bitcababy+github@gmail.com>
2011-11-19 14:24:06 -0500

    Removed fossil tests,

--- spec/datatable/request_spec.rb
+++ spec/datatable/request_spec.rb
@@ -2,17 +2,33 @@ require 'spec_helper'
 
 describe Datatable::Request do
 	include DatatableHelperMethods
+	
+	def make_searchable(opts)
+		opts["iSearchCols"] = "1"
+		opts["sSearch"] = ""
+		opts["bSearchable_0"] = "true"
+		opts["sSearch_0"] = "foo"
+	end
+	
+	def make_sortable(opts)
+		opts["iSortingCols"] = "1"
+		opts["iSortCol_0"] = 0
+		opts["sSortDir_0"] = "asc"
+	end
+	
 	before :each do
 		@columns = %W(problem year meet_round number)
 		@opts = create_opts(@columns)
 	end
 		
-	context "initialization" do
+	context "initialization methods" do
 		describe "#initialize_basics" do
 			before :each do
 				@opts['iDisplayStart'] = "0"
 				@opts['iDisplayLength'] = "10"
-				@obj = Datatable::Request.new(@opts)
+				@opts["iSortingCols"] = "0"
+				@obj = Datatable::Request.new(@opts, false)
+				@obj.initialize_basics
 			end
 	
 			it "sets opts" do
@@ -32,7 +48,7 @@ describe Datatable::Request do
 			end
 	
 			it "sets iColumns" do
-				@obj.iColumns.should == @columns.count
+				@obj.iColumns.should == @columns.count - 1
 			end
 	
 			it "sets sColumns" do
@@ -46,10 +62,6 @@ describe Datatable::Request do
 			it "sets iSortingCols" do
 				@obj.iSortingCols.should == 0
 			end
-		
-			it "shouldn't be sortable" do
-				@obj.sortable?.should be_false
-			end
 		end
 	
 		describe "#initialize_sorts" do
@@ -61,10 +73,9 @@ describe Datatable::Request do
 				@opts["sSortDir_1"] = "desc"
 				@opts["bSortable_0"] = "true"
 				@opts["bSortable_2"] = "true"
-			
 				@obj = Datatable::Request.new(@opts)
 			end
-		
+	
 			it "creates bSortables" do
 				@obj.bSortables.should == [true, false, true, false]
 			end
@@ -72,159 +83,144 @@ describe Datatable::Request do
 			it "creates iSortCols" do
 				@obj.iSortCols.should == [0, 2]
 			end
-
+	
 			it "creates sSortDirs" do
 				@obj.sSortDirs.should == ["ASC", "DESC"]
 			end		
-		end #initialize_sorts
-		
-		describe "#initialize_searches" do
-			before :each do
-				@opts["bSearchable_0"] = "true"
-				@opts["sSearch_0"] = "foo"
-
-				@obj = Datatable::Request.new(@opts)
-			end
-		
-			it "creates bSearchables" do
-				@obj.bSearchables.should == [true, false, false, false]
-			end
-		
-			it "creates bSearches" do
-				@obj.bSearches.should == ["foo", "", "", ""]
-			end
-		
-			it "creates sRegexes" do
-				@obj.sRegexes.should == [false, false, false, false]
-			end
-		
-		end #initialize_searches
-	end #initialization
-	
-	context "searchable-related methods" do
-		describe "#searchable?" do
-			it "should return false if there's nothing to search for" do
-				@obj = Datatable::Request.new(@opts)
-				@obj.searchable?.should be_false
-			end
-			it "should return true if any bSearchables is true and the search string isn't empty" do
-				make_searchable(@opts)
-				@obj = Datatable::Request.new(@opts)
-				@obj.searchable?.should be_true
-			end
-			it "should return true if sSearch isn't empty" do
-				@opts["sSearch"] = "foo"
-				@obj = Datatable::Request.new(@opts)
-				@obj.searchable?.should be_true
-			end
-		end
-	end
-
-	describe "#sortable?" do
-		it "returns true if there's a sortable column" do
-			make_sortable(@opts)
-			@obj = Datatable::Request.new(@opts)
-			@obj.sortable?.should be_true
-		end
-	end
-
-	describe "#construct_conditions" do
-		it "constructs a WHEREable string" do
-			@opts["sSearch_0"] = "foo"
-			@opts["bSearchable_0"] = "true"
-			@opts["bRegex_0"] = "false"
-		
-			@obj = Datatable::Request.new(@opts)
-			@obj.construct_conditions.should == "problem LIKE 'foo'"
-		end
-		it "constructs a WHEREable string" do
-			@opts["sSearch_0"] = "foo"
-			@opts["bSearchable_0"] = "true"
-			@opts["bRegex_0"] = "true"
-		
-			@obj = Datatable::Request.new(@opts)
-			@obj.construct_conditions.should == "problem ~ 'foo'"
-		end
-	end
-
-	describe "#construct_order" do
-		it "creates an SQL fragment for the ORDER clause" do
-			make_sortable(@opts)
-			@obj = Datatable::Request.new(@opts)
-			@obj.construct_order.should == "#{@columns[@opts['iSortCol_0']]} #{@opts['sSortDir_0'].upcase}"
-		end
-		it "uses overrides when provided" do
-			@opts["sSort_1"] = "meet DIR, round DIR"
-			@opts["iSortingCols"] = 2
-			@opts["iSortCols"] = 
-			@opts["iSortCol_0"] = 1
-			@opts["sSortDir_0"] = "asc"
-			@opts["iSortCol_1"] = 2
-			@opts["sSortDir_1"] = "asc"
-			@obj = Datatable::Request.new(@opts)
-			@obj.construct_order.should =~ /year ASC,\s*meet ASC,\s*round ASC/
-		end
-	end
-
-	describe "#construct_select" do
-		it "should be return a comma-delimited list of columns if there are no overrides" do
-			@obj = Datatable::Request.new(@opts)
-			@obj.construct_select.should == @columns.join(',')
-		end
-		it "should look for sSelect_x and fix up the select list" do
-			@opts["sColumns"] = "problem,year,meet_and_round,number"
-			@opts['sSelect_2'] = 'meet,round'
-			@obj = Datatable::Request.new(@opts)
-			@obj.construct_select.should == "problem,year,meet,round,number"
-		end
-	end
-
-	describe "#args_for_find" do
-		before :each do
-			@obj = Datatable::Request.new(@opts)
-			@args = @obj.args_for_find
-		end
-
-		describe ":limit" do
-			it "should be an integer" do
-				@args[:limit].should be_kind_of Integer
-			end
-		end
-	
-		describe ":offset" do
-			it "should be an integer" do
-				@args[:offset].should be_kind_of Integer
-			end
-		end
-	
-		describe ":conditions" do
-			it "shouldn't exist if not searchable" do
-				@args[:conditions].should be_nil
-			end
-
-			it "should exist if searchable" do
-				make_searchable(@opts)
-				@obj = Datatable::Request.new(@opts)
-				@obj.searchable?.should be_true
-				@args = @obj.args_for_find
-				@args[:conditions].should be_kind_of String
-			end
-		end
-	
-		describe ":construct_order" do
-			it "shouldn't exist if not sortable" do
-				@obj = Datatable::Request.new(@opts)
-				@args = @obj.args_for_find
-				@args[:order].should be_nil
-			end
-		
-			it "should contain a string if sortable" do
-				make_sortable(@opts)
-				@obj = Datatable::Request.new(@opts)
-				@args = @obj.args_for_find
-				@args[:order].should be_kind_of String
-			end
-		end
-	end
+			end #initialize_sorts
 			
+			describe "#initialize_searches" do
+				before :each do
+					@opts["bSearchable_0"] = "true"
+					@opts["sSearch_0"] = "foo"
+		
+					@obj = Datatable::Request.new(@opts)
+					@obj.initialize_basics
+					@obj.initialize_searches
+				end
+			
+				it "creates bSearchables" do
+					@obj.bSearchables.should == [true, false, false, false]
+				end
+			
+				it "creates bSearches" do
+					@obj.bSearches.should == ["foo", "", "", ""]
+				end
+			
+				it "creates sRegexes" do
+					@obj.sRegexes.should == [false, false, false, false]
+				end
+			end #initialize_searches
+			
+			describe "#expand_opts" do
+				before :each do
+					@obj = Datatable::Request.new(@opts)
+				end
+				it "should call each of the initialize methods" do
+					[:initialize_basics, :initialize_sorts].each do |meth|
+						@obj.should respond_to(meth)
+					end
+					@obj.expand_opts
+				end
+			end
+		end #initialization
+		
+		
+		# describe "#construct_conditions" do
+		# 	it "constructs a WHEREable string" do
+		# 		@opts["sSearch_0"] = "foo"
+		# 		@opts["bSearchable_0"] = "true"
+		# 		@opts["bRegex_0"] = "false"
+		# 	
+		# 		@obj = Datatable::Request.new(@opts)
+		# 		@obj.construct_conditions.should == "problem LIKE 'foo'"
+		# 	end
+		# 	it "constructs a WHEREable string" do
+		# 		@opts["sSearch_0"] = "foo"
+		# 		@opts["bSearchable_0"] = "true"
+		# 		@opts["bRegex_0"] = "true"
+		# 	
+		# 		@obj = Datatable::Request.new(@opts)
+		# 		@obj.construct_conditions.should == "problem ~ 'foo'"
+		# 	end
+		# end
+		# 
+		# describe "#construct_select" do
+		# 	it "should be return a comma-delimited list of columns if there are no overrides" do
+		# 		@obj = Datatable::Request.new(TestModel, @opts)
+		# 		@obj.construct_select.should == @columns.join(',')
+		# 	end
+		# 
+		# 	it "should look for replacements and fix up the select list" do
+		# 		@opts["sColumns"] = "problem,year,meet_and_round,number"
+		# 		@obj = Datatable::Request.new(TestModel, @opts)
+		# 		@obj.construct_select.should == "problem,year,meet,round,number"
+		# 	end
+		# end
+		# 
+		# describe "#construct_order" do
+		# 	it "creates an SQL fragment for the ORDER clause" do
+		# 		make_sortable(@opts)
+		# 		@obj = Datatable::Request.new(TestModel, @opts)
+		# 		@obj.construct_order.should == "#{@columns[@opts['iSortCol_0']]} #{@opts['sSortDir_0'].upcase}"
+		# 	end
+		# 
+		# 	it "uses overrides when provided" do
+		# 		@opts["iSortingCols"] = 2
+		# 		@opts["iSortCol_0"] = 1
+		# 		@opts["sSortDir_0"] = "asc"
+		# 		@opts["iSortCol_1"] = 2
+		# 		@opts["sSortDir_1"] = "asc"
+		# 		@obj = Datatable::Request.new(TestModel, @opts)
+		# 		@obj.construct_order.should =~ /year ASC,\s*meet ASC,\s*round ASC/
+		# 	end
+		# end
+		# 
+		# describe "#args_for_find" do
+		# 	before :each do
+		# 		@obj = Datatable::Request.new(TestModel, @opts)
+		# 		@args = @obj.args_for_find
+		# 	end
+		# 
+		# 	describe ":limit" do
+		# 		it "should be an integer" do
+		# 			@args[:limit].should be_kind_of Integer
+		# 		end
+		# 	end
+		# 
+		# 	describe ":offset" do
+		# 		it "should be an integer" do
+		# 			@args[:offset].should be_kind_of Integer
+		# 		end
+		# 	end
+		# 
+		# 	describe ":conditions" do
+		# 		it "shouldn't exist if not searchable" do
+		# 			@args[:conditions].should be_nil
+		# 		end
+		# 
+		# 		it "should exist if searchable" do
+		# 			make_searchable(@opts)
+		# 			@obj = Datatable::Request.new(@opts)
+		# 			@obj.searchable?.should be_true
+		# 			@args = @obj.args_for_find
+		# 			@args[:conditions].should be_kind_of String
+		# 		end
+		# 	end
+		# 
+		# 	describe ":construct_order" do
+		# 		it "shouldn't exist if not sortable" do
+		# 			@obj = Datatable::Request.new(@opts)
+		# 			@args = @obj.args_for_find
+		# 			@args[:order].should be_nil
+		# 		end
+		# 	
+		# 		it "should contain a string if sortable" do
+		# 			make_sortable(@opts)
+		# 			@obj = Datatable::Request.new(@opts)
+		# 			@args = @obj.args_for_find
+		# 			@args[:order].should be_kind_of String
+		# 		end
+		# 	end
+				
 end
